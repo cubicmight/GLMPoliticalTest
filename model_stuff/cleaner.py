0052564919ade5cc7101cc9046fc264686121c4b
Sankalp Singh <838sanky@gmail.com>
2023-05-31 19:38:02 -0400

    second commit

---
+++ model_stuff/cleaner.py
@@ -0,0 +1,39 @@
+import os
+import nltk
+nltk.download('words')
+
+# Load English word dictionary
+english_words = set(nltk.corpus.words.words())
+
+# Function to check if a phrase is composed of English words
+def is_english(phrase):
+    words = phrase.split()
+    return all(word.lower() in english_words for word in words)
+
+# Directory paths
+dataset_directory = '../dataset'
+cleaned_data_directory = '../cleaned_data'
+
+# Create the cleaned_data directory if it doesn't exist
+if not os.path.exists(cleaned_data_directory):
+    os.makedirs(cleaned_data_directory)
+
+# Iterate over files in the dataset directory
+for filename in os.listdir(dataset_directory):
+    if filename.startswith('partisan_phrases'):
+        # Read data from file
+        file_path = os.path.join(dataset_directory, filename)
+        with open(file_path, 'r') as file:
+            data = file.readlines()
+
+        # Clean the data
+        cleaned_data = []
+        for line in data:
+            phrase, _ = line.strip().split('|')
+            if is_english(phrase):
+                cleaned_data.append(line)
+
+        # Write cleaned data to a new file
+        cleaned_file_path = os.path.join(cleaned_data_directory, f'cleaned_{filename}')
+        with open(cleaned_file_path, 'w') as file:
+            file.writelines(cleaned_data)
